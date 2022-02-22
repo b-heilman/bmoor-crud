1bf0d4cf03b91eb00c55a6d6abf2e3d9d46be4c5
Brian Heilman <brian.heilman@gmail.com>
2022-02-22 18:22:39 -0500

    need to remove, can stub

--- src/integration.spec.js
+++ src/integration.spec.js
@@ -2,7 +2,6 @@
 const {expect} = require('chai');
 const sinon = require('sinon');
 const express = require('express');
-const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 
 describe('integration tests', function(){
